f3806fc2ca2f85018b81fcca2861b642894f46ea
Cale Teeter <cale.teeter@microsoft.com>
2022-08-22 16:28:48 -0400

    Updated to add mi

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,8 +4,11 @@ managedIdentity=$1
 resourceGroupName=$2
 serverName=$3
 dbName=$4
+subscriptionId=$5
 
 # login
 az login --identity --username $managedIdentity --allow-no-subscriptions
+
+az account set --subscription $subscriptionId
 
 # az postgres db create --name $dbName --resource-group $resourceGroupName --server-name $serverName

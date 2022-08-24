3e413bb2bb63df7ecfee7362b4c51efbeca6e140
Cale Teeter <cale.teeter@microsoft.com>
2022-08-24 11:25:10 -0400

    Update'

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,8 +8,8 @@ serverName=$3
 az login --identity --username $managedIdentity
 
 # create databases
-az postgres db create --name 'domain1' --resource-group $resourceGroupName --server-name $serverName
-az postgres db create --name 'domain2' --resource-group $resourceGroupName --server-name $serverName
-az postgres db create --name 'participant1' --resource-group $resourceGroupName --server-name $serverName
-az postgres db create --name 'participant2' --resource-group $resourceGroupName --server-name $serverName
-az postgres db create --name 'participant3' --resource-group $resourceGroupName --server-name $serverName
+# az postgres db create --name 'domain1' --resource-group $resourceGroupName --server-name $serverName
+# az postgres db create --name 'domain2' --resource-group $resourceGroupName --server-name $serverName
+# az postgres db create --name 'participant1' --resource-group $resourceGroupName --server-name $serverName
+# az postgres db create --name 'participant2' --resource-group $resourceGroupName --server-name $serverName
+# az postgres db create --name 'participant3' --resource-group $resourceGroupName --server-name $serverName

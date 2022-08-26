e354e2cbfddae12e5fb794b86305d41e8e33c8da
Cale Teeter <cale.teeter@microsoft.com>
2022-08-26 10:49:47 -0400

    Update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,13 +3,18 @@
 managedIdentity=$1
 resourceGroupName=$2
 serverName=$3
+administratorLogin=$4
+administratorLoginPassword=$5
 
 # login
 az login --identity --username $managedIdentity
 
 # create databases
-# az postgres db create --name 'domain1' --resource-group $resourceGroupName --server-name $serverName
-# az postgres db create --name 'domain2' --resource-group $resourceGroupName --server-name $serverName
-# az postgres db create --name 'participant1' --resource-group $resourceGroupName --server-name $serverName
-# az postgres db create --name 'participant2' --resource-group $resourceGroupName --server-name $serverName
-# az postgres db create --name 'participant3' --resource-group $resourceGroupName --server-name $serverName
+az postgres flexible-db create --name 'domain1' --resource-group $resourceGroupName --server-name $serverName
+az postgres flexible-db create --name 'domain2' --resource-group $resourceGroupName --server-name $serverName
+az postgres flexible-db create --name 'participant1' --resource-group $resourceGroupName --server-name $serverName
+az postgres flexible-db create --name 'participant2' --resource-group $resourceGroupName --server-name $serverName
+az postgres flexible-db create --name 'participant3' --resource-group $resourceGroupName --server-name $serverName
+
+# create users
+az postgres flexible-db execute --admin-user $administratorLogin --admin-password $administratorLoginPassword -name $serverName --database-name 'domain1' --querytext 'create user domain1 with password "P@ssw0rd123!"'

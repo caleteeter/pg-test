c2700e6b737c31947306024dc00c35a08558d150
Cale Teeter <cale.teeter@microsoft.com>
2022-08-26 11:18:08 -0400

    Update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,11 +10,11 @@ administratorLoginPassword=$5
 az login --identity --username $managedIdentity
 
 # create databases
-az postgres flexible-db create --name 'domain1' --resource-group $resourceGroupName --server-name $serverName
-az postgres flexible-db create --name 'domain2' --resource-group $resourceGroupName --server-name $serverName
-az postgres flexible-db create --name 'participant1' --resource-group $resourceGroupName --server-name $serverName
-az postgres flexible-db create --name 'participant2' --resource-group $resourceGroupName --server-name $serverName
-az postgres flexible-db create --name 'participant3' --resource-group $resourceGroupName --server-name $serverName
+az postgres flexible-server create --name 'domain1' --resource-group $resourceGroupName --server-name $serverName
+az postgres flexible-server create --name 'domain2' --resource-group $resourceGroupName --server-name $serverName
+az postgres flexible-server create --name 'participant1' --resource-group $resourceGroupName --server-name $serverName
+az postgres flexible-server create --name 'participant2' --resource-group $resourceGroupName --server-name $serverName
+az postgres flexible-server create --name 'participant3' --resource-group $resourceGroupName --server-name $serverName
 
 # create users
-az postgres flexible-db execute --admin-user $administratorLogin --admin-password $administratorLoginPassword -name $serverName --database-name 'domain1' --querytext 'create user domain1 with password "P@ssw0rd123!"'
+az postgres flexible-server execute --admin-user $administratorLogin --admin-password $administratorLoginPassword -name $serverName --database-name 'domain1' --querytext 'create user domain1 with password "P@ssw0rd123!"'

e0fe833986e24548b661a315adb967f4d1b057dd
Cale Teeter <cale.teeter@microsoft.com>
2022-08-29 09:40:15 -0400

    Update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,7 +10,7 @@ administratorLoginPassword=$5
 az login --identity --username $managedIdentity
 
 # ensure the preview bits can be used with prompt in UI
-az config set extension.use_dynamic_install=yes_without_prompt
+az extension add --name rdbms-connect
 
 # create databases
 az postgres flexible-server db create --database-name 'domain1' --resource-group $resourceGroupName --server-name $serverName

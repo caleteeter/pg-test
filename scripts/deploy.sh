53d8c76d6f156b7612dcd1b908134c2703f787dc
Cale Teeter <cale.teeter@microsoft.com>
2022-08-30 14:33:08 -0400

    update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,7 +11,6 @@ az login --identity --username $managedIdentity
 
 # ensure the preview bits can be used with prompt in UI
 az config set extension.use_dynamic_install=yes_without_prompt
-az extension add -s https://azurecliprod.blob.core.windows.net/cli-extensions/rdbms_connect-0.1.0-py2.py3-none-any.whl -y
 
 # create databases
 az postgres flexible-server db create --charset 'UTF8' --collation "en_US.UTF8" --database-name 'domain1' --resource-group $resourceGroupName --server-name $serverName
@@ -21,4 +20,4 @@ az postgres flexible-server db create --charset 'UTF8' --collation "en_US.UTF8" 
 az postgres flexible-server db create --charset 'UTF8' --collation "en_US.UTF8" --database-name 'participant3' --resource-group $resourceGroupName --server-name $serverName
 
 # create users
-az postgres flexible-server execute --admin-user $administratorLogin --admin-password $administratorLoginPassword --name $serverName --database-name 'domain1' --querytext "create user domain1 with password 'P@ssw0rd123!'"
+az postgres flexible-server execute --debug --admin-user $administratorLogin --admin-password $administratorLoginPassword --name $serverName --database-name 'domain1' --querytext "create user domain1 with password 'P@ssw0rd123!'"

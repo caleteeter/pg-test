f82034739edf6dd88aaa19bf8df371ee381e37a7
Cale Teeter <cale.teeter@microsoft.com>
2022-08-31 00:31:42 -0400

    Update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,6 +12,9 @@ az login --identity --username $managedIdentity
 # ensure the preview bits can be used with prompt in UI
 az config set extension.use_dynamic_install=yes_without_prompt
 
+# install the psql client
+apk --no-cache add postgresql-client
+
 # create databases
 az postgres flexible-server db create --charset 'UTF8' --collation "en_US.UTF8" --database-name 'domain1' --resource-group $resourceGroupName --server-name $serverName
 az postgres flexible-server db create --charset 'UTF8' --collation "en_US.UTF8" --database-name 'domain2' --resource-group $resourceGroupName --server-name $serverName
@@ -20,8 +23,6 @@ az postgres flexible-server db create --charset 'UTF8' --collation "en_US.UTF8" 
 az postgres flexible-server db create --charset 'UTF8' --collation "en_US.UTF8" --database-name 'participant3' --resource-group $resourceGroupName --server-name $serverName
 
 # create users
-# psql "postgres://$administratorLogin@$serverName:$administratorLoginPassword@$serverName.postgres.database.azure.com/postgres" -c "CREATE DATABASE northwind;"
-
-apk --no-cache add postgresql-client
+psql "postgres://$administratorLogin@$serverName:$administratorLoginPassword@$serverName.postgres.database.azure.com/postgres" -c "CREATE DATABASE northwind;"
 
 # az postgres flexible-server execute --debug --admin-user $administratorLogin --admin-password $administratorLoginPassword --name $serverName --database-name 'domain1' --querytext "create user domain1 with password 'P@ssw0rd123!'"

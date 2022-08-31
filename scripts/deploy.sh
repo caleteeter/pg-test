957b25e943c335b80aab074079f5ffb03d5bcd9c
Cale Teeter <cale.teeter@microsoft.com>
2022-08-31 15:36:25 -0400

    Update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -24,6 +24,6 @@ az postgres flexible-server db create --charset 'UTF8' --collation "en_US.UTF8" 
 az postgres flexible-server db create --charset 'UTF8' --collation "en_US.UTF8" --database-name 'participant3' --resource-group $resourceGroupName --server-name $serverName
 
 # create users
-psql "host=$serverName port=5432 dbname=postgres user=$administratorLogin sslmode=require" -c "CREATE DATABASE ctest;"
+psql "host=$serverName.postgres.database.azure.com port=5432 dbname=postgres user=$administratorLogin sslmode=require" -c "CREATE DATABASE ctest;"
 
 # az postgres flexible-server execute --debug --admin-user $administratorLogin --admin-password $administratorLoginPassword --name $serverName --database-name 'domain1' --querytext "create user domain1 with password 'P@ssw0rd123!'"
